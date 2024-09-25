df5e1361ee4445f5d5bf57a673ab56aa5188a55c
Chris <cgonzalez83@cnm.edu>
2024-09-25 11:26:11 -0600

    Changed local class to anonymous class

--- src/test/java/edu/cnm/deepdive/FizzBuzzTest.java
+++ src/test/java/edu/cnm/deepdive/FizzBuzzTest.java
@@ -4,22 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 class FizzBuzzTest {
 
-  @Test
-  void valueOf_negativeInput() {
-  assertThrows(IllegalArgumentException.class, new NegativeFizzBuzzExecutable());
+  @ParameterizedTest
+  @ValueSource(ints = {-1, -2, -15, Integer.MIN_VALUE})
+  void valueOf_negativeInput(int value) {
+    Executable task = new Executable() {
+
+      @Override
+      public void execute() throws Throwable {
+        FizzBuzz fb = new FizzBuzz();
+        fb.valueOf(value);
+      }
+
+    };
+  assertThrows(IllegalArgumentException.class, task);
     }
 
   }
 
-class NegativeFizzBuzzExecutable implements Executable {
-
-
-  @Override
-  public void execute() throws Throwable {
-    FizzBuzz fb = new FizzBuzz();
-    fb.valueOf(-1);
-  }
-}
